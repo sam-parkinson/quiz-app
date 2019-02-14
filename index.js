880cfab4849cf151dbe15dc90440dd6a6d9491c5
Sam Parkinson <parkinsonsp42@gmail.com>
2019-02-14 18:57:05 -0500

    added pseudocode and dummy functions, began js implementation

---
+++ index.js
@@ -0,0 +1,36 @@
+function startButtonClicked() {
+  $('.js-start').on('click', event =>{
+    
+  });
+  // the quiz will begin
+  // set score variable to zero
+  // bring up the first question
+  // return score, so other functions can manipulate it
+}
+
+function questionAnswered() {
+  // check if the answer is correct or not
+  // bring up the next question
+}
+
+function answerCorrect() {
+  // increment score
+  // display score
+  // inform user that answer is correct
+  // return score
+}
+
+function answerIncorrect() {
+  // inform user that answer is incorrect
+  // inform user of correct answer
+}
+
+function getNextQuestion() {
+  // gets the next question, displays it
+  // shows answers
+  // adds hidden classes indicating correctness and incorrectness
+}
+
+function restartQuiz() {
+  // do I need a separate function for this? maybe reuse start function?
+}
